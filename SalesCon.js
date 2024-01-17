f892282143513a185370621a4b4d29212c64865b
Ashish-Kaintura <ashu88793@gmail.com>
2024-01-16 22:20:55 -0800

    second  commit

---
+++ SalesCon.js
@@ -0,0 +1,19 @@
+var tl = gsap.timeline();
+tl.from("#headnav .nav1, #headnav .nav2, #headnav .nav3, #headnav .nav4", {
+  y: 100,
+  duration: 1,
+  delay: 0.5,
+  opacity: 0,
+  stagger: 0.5,
+  // scrollTrigger:{
+  //   trigger:"#headnav .nav1, #headnav .nav2, #headnav .nav3, #headnav .nav4",
+  //   scroller:"body"
+  // }
+});
+tl.from("#sec1 .div1, #sec1 .h1", {
+  scale: 4,
+  duration: 1,
+  delay: 0.5,
+  opacity: 0,
+  stagger: 0.5,
+});
